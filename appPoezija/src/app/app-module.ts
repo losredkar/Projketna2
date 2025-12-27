9849f5793577d7ac0f261c8c9e35ddf431dbdbe1
losredkar <losredkar@gmail.com>
2025-12-27 17:12:12 +0100

    Dodan bootstrap + prijavna stran(osnova) + registracijska stran(osnova) + footer  + header + testna navigacija

--- appPoezija/src/app/app-module.ts
+++ appPoezija/src/app/app-module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Footer } from './Shared/Components/footer/footer';
+import { Header } from './Shared/Components/header/header';
+import { Login } from './Features/Authentication/Pages/login/login';
+import { SignUp } from './Features/Authentication/Pages/sign-up/sign-up';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Footer,
+    Header,
+    Login,
+    SignUp
   ],
   imports: [
     BrowserModule,
--- appPoezija/src/app/app-routing-module.ts
+++ appPoezija/src/app/app-routing-module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Login } from './Features/Authentication/Pages/login/login';
+import { SignUp } from './Features/Authentication/Pages/sign-up/sign-up';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo: '/login', pathMatch: 'full'},
+  {path: 'login', component: Login},
+  {path: 'signup', component: SignUp},
+  {path: '**', redirectTo: '/login'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
